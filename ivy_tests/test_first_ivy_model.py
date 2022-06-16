66a36bbe69e6fbd6595c108264531ea3e10f6610
Tetsuo Seto <setogit@gmail.com>
2022-06-16 15:03:56 -0700

    Add first ivy model training example   - WIP: tenforflow generates a few errors   - WIP: mxnet train fails due to recursive too deep   - TODO: loss_fn calls ivy.mean to workaroud missing ivy.reduce_mean   - Note: ivy-core 1.1.10 on pypi.org missing ivy.set_backend   - Note: processing time torch 2.1 sec, jax 6.1 sec, tensorflow 5.5 sec

---
+++ ivy_tests/test_first_ivy_model.py
@@ -0,0 +1,44 @@
+# global
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import time
+import pytest
+import ivy
+
+@pytest.mark.parametrize("backend", ["torch", "jax", "tensorflow", "mxnet"])
+def test_first_ivy_model(backend):
+# TODO: make "tensorflow" and "mxnet" training work
+
+    class MyModel(ivy.Module):
+        def __init__(self):
+            self.linear0 = ivy.Linear(3, 64)
+            self.linear1 = ivy.Linear(64, 1)
+            ivy.Module.__init__(self)
+
+        def _forward(self, x):
+            x = ivy.relu(self.linear0(x))
+            return ivy.sigmoid(self.linear1(x))
+
+    start = time.time()
+    ivy.set_default_device("cpu")
+    ivy.set_backend(backend)  # change to any framework!
+    model = MyModel()
+    optimizer = ivy.Adam(1e-4)
+    x_in = ivy.array([1., 2., 3.])
+    target = ivy.array([0.])
+
+    def loss_fn(v):
+        # TODO: fix this loss function
+        # It used to be ivy.reduce_mean((out - target)**2)[0]
+        out = model(x_in, v=v)
+        return ivy.mean((out - target)**2)
+
+    for step in range(100):
+        loss, grads = ivy.execute_with_gradients(loss_fn, model.v)
+        model.v = optimizer.step(model.v, grads)
+        print('step {} loss {}'.format(step, ivy.to_numpy(loss).item()))
+
+    ivy.unset_backend()
+    elapsed_msec = (time.time() - start)*1_000
+    print(f'Finished training with {backend} in {elapsed_msec:,.2f} msec.')
