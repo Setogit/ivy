7674893ed31760c09ea2def25e57a1920e2f37a2
Tetsuo Seto <setogit@gmail.com>
2022-10-12 21:07:18 -0700

    Update sh to follow the new pytest directory structure

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_first_ivy_model.py -s
+docker run --rm -it -v "$(pwd)":/ivy unifyai/ivy:latest python3 -m pytest ivy_tests/test_first_ivy_model.py -s
